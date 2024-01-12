8b8a4a88ea5c074a2ecde00b86d77b8f8ae15cdf
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2024-01-12 08:46:12 +0000

    new repo

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,7 @@ import ClickCount from '../components/ClickCount'
 import styles from '../styles/home.module.css'
 
 function throwError() {
-  console.log(
-    // The function body() is not defined
-    document.body()
-  )
+ 
 }
 
 function Home() {
